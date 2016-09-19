3d0fb0b5286ec1d9f5d9be3d974467d628dbd5a8
Arkadiusz Zalewski <arek@yaska.eu>
2016-09-19 17:52:25 +0200

    Added collapsing form sections

--- fields/types/heading/HeadingField.js
+++ fields/types/heading/HeadingField.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classnames'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import Field from '../Field';
 import evalDependsOn from '../../utils/evalDependsOn';
 
@@ -10,6 +12,9 @@ module.exports = Field.create({
 	statics: {
 		type: 'Heading',
 	},
+	toggleSection: function () {
+		this.setState({collapsed: !this.state.collapsed})
+	},
 	renderField () {
 		const {
 			options: { dependsOn, heading, collapsible },
@@ -20,9 +25,27 @@ module.exports = Field.create({
 			return null;
 		}
 		return (
-			<section>
-				<h3 className="form-heading">[TODO collapse {`${collapsed}/${collapsible}`}] {heading}</h3>
-				{children}
+			<section className="collapsing-section">	
+				<div className="section-heading" onClick={this.toggleSection}>
+					<span className={
+						classNames(
+							'octicon octicon-triangle-right arrow-icon',
+							{rotateDown: !this.state.collapsed}
+						)}
+					/>
+					<h3 className="form-heading">[TODO collapse {`${collapsed}/${collapsible}`}] {heading}</h3>
+				</div>
+				<ReactCSSTransitionGroup 
+					transitionName="toggleDropdown" 
+					transitionEnterTimeout={330} 
+					transitionLeaveTimeout={330}
+				>
+					{collapsed || 
+						<div className="form-content">
+							{children}
+						</div>
+					}
+				</ReactCSSTransitionGroup>
 			</section>
 		);
 	},
