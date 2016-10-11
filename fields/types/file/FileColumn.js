448aaa508168beb74edac1057bb233bb77f6805d
Wout Mertens <Wout.Mertens@gmail.com>
2016-10-11 10:49:40 +0200

    FileColumn: Use original filename if available

--- fields/types/file/FileColumn.js
+++ fields/types/file/FileColumn.js
@@ -6,13 +6,13 @@ import ItemsTableValue from '../../components/ItemsTableValue';
 var LocalFileColumn = React.createClass({
 	renderValue: function () {
 		var value = this.props.data.fields[this.props.col.path];
-		if (!value || !value.filename) return;
-		return value.filename;
+		if (!value) return;
+		return value.originalname || value.filename;
 	},
 	render: function () {
 		var value = this.props.data.fields[this.props.col.path];
 		var href = value && value.url ? value.url : null;
-		var label = value && value.filename ? value.filename : null;
+		var label = value && (value.originalname || value.filename);
 		return (
 			<ItemsTableCell href={href} padded interior field={this.props.col.type}>
 				<ItemsTableValue>{label}</ItemsTableValue>
